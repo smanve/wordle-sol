4ca7b920b1df1636ce0ca1c6a1af738583b416e7
Manvendra Singh <msin0017@student.monash.edu>
2023-05-19 00:27:41 +1000

    getting user input

--- wordle-solve.cpp
+++ wordle-solve.cpp
@@ -1,19 +1,37 @@
 // Need to make a C++ Wordle Solver
 
-#Include<iostream.h>
-#Include<string.h>
-#Include<vector>
-#Include<fstream>
+#include<iostream.h>
+#include<string.h>
+#include<math.h>
+#include<stdio.h>
+#include<vector>
+#include<fstream>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
 ifstream inFile;   
+string guess;
+string feedback;
 inFile.open("words.txt");
 
-if (!inFile) {
-    cerr << "Unable to open file datafile.txt";
-    exit(1);   // call system to stop
-}
+if (inFile.is_open()) {
+        string word;
+        while (getline(file, word)) {
+            words.push_back(word);
+        }
+        file.close();
+    } else {
+        std::cout << "Failed to open the file: " << filename << std::endl;
+    }
+std::cout << "Welcome to Wordle Solver!\n";
+std::cout << "Enter your guess: ";
+std::cin >> guess;
 
+std::cout << "Enter the feedback: ";
+std::cin >> feedback;
+
+transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+transform(feedback.begin(), feedback.end(), feedback.begin(), ::tolower);
 }
