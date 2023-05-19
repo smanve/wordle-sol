237669d873e295a73ae790938048d1e9c4e9361f
Manvendra Singh <msin0017@student.monash.edu>
2023-05-19 14:01:40 +1000

    matching words need to refactored

--- wordle-solve.cpp
+++ wordle-solve.cpp
@@ -1,37 +1,77 @@
-// Need to make a C++ Wordle Solver
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-#include<iostream.h>
-#include<string.h>
-#include<math.h>
-#include<stdio.h>
-#include<vector>
-#include<fstream>
-#include<algorithm>
+// Function to check if a word matches the hints
+bool matchesHints(const std::string& word, const std::string& guess, const std::string& feedback) {
+    std::string tempFeedback = feedback;
 
-using namespace std;
+    // Check for correct letters at correct positions
+    for (int i = 0; i < guess.size(); ++i) {
+        if (guess[i] == word[i]) {
+            tempFeedback[i] = '*';
+        }
+    }
 
-int main(){
-ifstream inFile;   
-string guess;
-string feedback;
-inFile.open("words.txt");
+    // Check for correct letters at incorrect positions
+    for (int i = 0; i < guess.size(); ++i) {
+        if (tempFeedback[i] != '*' && word[i] != guess[i]) {
+            tempFeedback[i] = '#'; // Use '#' to represent incorrect letters
+        }
+    }
 
-if (inFile.is_open()) {
-        string word;
-        while (getline(file, word)) {
+    // Compare the feedback with the given feedback
+    return tempFeedback == feedback;
+}
+
+int main() {
+    std::ifstream file("words.txt");
+    std::string guess;
+    std::string feedback;
+    std::vector<std::string> words;
+
+    if (file.is_open()) {
+        std::string word;
+        while (std::getline(file, word)) {
             words.push_back(word);
         }
         file.close();
     } else {
-        std::cout << "Failed to open the file: " << filename << std::endl;
+        std::cout << "Failed to open the file." << std::endl;
+        return 1;
     }
-std::cout << "Welcome to Wordle Solver!\n";
-std::cout << "Enter your guess: ";
-std::cin >> guess;
 
-std::cout << "Enter the feedback: ";
-std::cin >> feedback;
+    std::cout << "Welcome to Wordle Solver! Please enter your first guess, then enter your feedback." << std::endl;
+    std::cout << "* (asterisk): This symbol represents a correct letter in the correct position." << std::endl;
+    std::cout << "# (hash): This symbol represents an incorrect letter." << std::endl;
 
-transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
-transform(feedback.begin(), feedback.end(), feedback.begin(), ::tolower);
+    std::cout << "Enter your guess: ";
+    std::cin >> guess;
+
+    std::cout << "Enter the feedback: ";
+    std::cin >> feedback;
+
+    transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+
+    // Filter the words based on the hints
+    std::vector<std::string> matchingWords;
+    for (const std::string& word : words) {
+        std::string lowerWord = word;
+        transform(lowerWord.begin(), lowerWord.end(), lowerWord.begin(), ::tolower);
+        if (matchesHints(lowerWord, guess, feedback)) {
+            matchingWords.push_back(word);
+        }
+    }
+
+    // Print the matching words
+    std::cout << "\nMatching words: ";
+    if (!matchingWords.empty()) {
+        std::copy(matchingWords.begin(), matchingWords.end() - 1, std::ostream_iterator<std::string>(std::cout, ", "));
+        std::cout << matchingWords.back();
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
